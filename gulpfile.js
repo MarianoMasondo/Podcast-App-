a4420ef3df109cdf110a94f92000ad2ca5e9e128
Mariano Masondo <nanomason@gmail.com>
2024-02-01 18:03:18 -0300

    Image optimization Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 const { src, dest, watch } = require("gulp");
+
+//compilar css
 const sass = require("gulp-sass")(require("sass"))
+
+//imagenes
+const imagemin = require('gulp-imagemin');
 
 function css(done){
     src("src/scss/app.scss") //identificar el archivo principal
@@ -10,7 +15,16 @@ function css(done){
 }
 
 function dev(){
-    watch("src/scss/a**/*.scss", css)
+    watch("src/scss/**/*.scss", css)
 }
+
+function imagenes(done) {
+    src('src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'))
+    done();
+}
+
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
